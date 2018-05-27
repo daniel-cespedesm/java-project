093753c5495c38ac3d9cc62c09931bd62cd754ad
Cespedes <jose-daniel.cespedes@hpe.com>
2018-05-27 14:35:09 -0600

    assertEquals import fixed in Rectangle.Test.java file

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -1,7 +1,8 @@
 import org.junit.Test;
-import static.org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class RectangleTest {
+
   Rectangle myRectangle = new Rectangle (5, 6);
 
   @Test
